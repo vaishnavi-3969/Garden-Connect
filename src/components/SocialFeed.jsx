f9018d79b3e072559a45245f90ab92c7140572d7
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-06-19 04:28:28 +0530

    updated

--- src/components/SocialFeed.jsx
+++ src/components/SocialFeed.jsx
@@ -1,42 +1,70 @@
-import React from 'react';
-
-const Marketplace = () => {
-  const products = [
-    {
-      id: 1,
-      name: 'Product 1',
-      price: 9.99,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    },
-    {
-      id: 2,
-      name: 'Product 2',
-      price: 19.99,
-      description: 'Praesent id est ut tortor accumsan laoreet in nec leo.',
-    },
-    {
-      id: 3,
-      name: 'Product 3',
-      price: 14.99,
-      description: 'Nulla ac erat sed felis feugiat pretium at nec eros.',
-    },
-    // Add more products as needed
-  ];
-
-  return (
-    <div className="marketplace-container">
-      <h1 className="marketplace-title">Marketplace</h1>
-      <div className="product-list">
-        {products.map((product) => (
-          <div className="product-card" key={product.id}>
-            <h2 className="product-name">{product.name}</h2>
-            <p className="product-description">{product.description}</p>
-            <p className="product-price">Price: ${product.price}</p>
+const SocialFeed = () => {
+    const feedItems = [
+      {
+        id: 1,
+        user: 'John Doe',
+        timestamp: '2 hours ago',
+        content: 'Just planted some beautiful flowers in my garden. Loving the vibrant colors!',
+        likes: 12,
+        comments: 5,
+      },
+      {
+        id: 2,
+        user: 'Jane Smith',
+        timestamp: '5 hours ago',
+        content: 'Harvested my first batch of tomatoes today. They taste amazing!',
+        likes: 24,
+        comments: 8,
+      },
+      {
+        id: 3,
+        user: 'Mike Johnson',
+        timestamp: '1 day ago',
+        content: 'Had a great time volunteering at the local community garden. We accomplished so much together!',
+        likes: 45,
+        comments: 15,
+      },
+      {
+        id: 4,
+        user: 'Mike Johnson',
+        timestamp: '2 days ago',
+        content: 'Volunteering at the local community garden.',
+        likes: 45,
+        comments: 15,
+      },
+    ];
+  
+    return (
+      <div className="social-feed bg-gradient-to-br from-purple-900 to-blue-900 text-white px-6 py-6">
+    <h2 className="text-3xl font-bold mb-4">Social Feeds</h2>
+      <div className="bg-gray-800 px-6 py-6">
+        {feedItems.map((item) => (
+          <div className="feed-item" key={item.id}>
+            <div className="feed-header">
+              <div className="user-avatar"></div>
+              <div className="user-info">
+                <p className="user-name">{item.user}</p>
+                <p className="timestamp">{item.timestamp}</p>
+              </div>
+            </div>
+            <div className="feed-content">
+              <p>{item.content}</p>
+            </div>
+            <div className="feed-footer">
+              <div className="likes">
+                <span className="count">{item.likes}</span> Likes
+              </div>
+              <div className="comments">
+                <span className="count">{item.comments}</span> Comments
+              </div>
+              <hr/>
+            </div>
           </div>
         ))}
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default Marketplace;
+    );
+  };
+  
+  export default SocialFeed;
+  
--- src/components/VirtualMarketplace.jsx
+++ src/components/VirtualMarketplace.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import { useState } from "react";
 
 const Marketplace = () => {
+  const [cart, setCart] = useState([]);
+
   const products = [
     {
       id: 1,
@@ -20,20 +22,42 @@ const Marketplace = () => {
       price: 14.99,
       description: 'Nulla ac erat sed felis feugiat pretium at nec eros.',
     },
-    // Add more products as needed
   ];
 
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
   return (
-    <div>
-      <h1>Marketplace</h1>
-      <div className="product-list">
+    <div className="marketplace-container bg-gradient-to-br from-gray-900 to-gray-700 text-white py-10">
+      <h1 className="marketplace-title text-4xl font-bold mb-6 text-center">Marketplace</h1>
+      <div className="product-list flex justify-center">
         {products.map((product) => (
-          <div key={product.id}>
-            <h2>{product.name}</h2>
-            <p>{product.description}</p>
-            <p>Price: ${product.price}</p>
+          <div className="product-card bg-gray-800 text-white rounded-lg p-4 mx-4 my-2" key={product.id}>
+            <h2 className="product-name text-xl font-bold mb-2">{product.name}</h2>
+            <p className="product-description mb-4">{product.description}</p>
+            <p className="product-price text-lg">${product.price}</p>
+            <button className="add-to-cart-button bg-blue-600 hover:bg-blue-700 text-white rounded-full py-2 px-4 mt-4"
+              onClick={() => addToCart(product)}
+            >
+              Add to Cart
+            </button>
           </div>
         ))}
+      </div>
+      <div className="cart-container text-center mt-10">
+        <h2 className="cart-title text-3xl font-bold mb-4">Cart</h2>
+        {cart.length === 0 ? (
+          <p className="empty-cart-message text-lg">Your cart is empty.</p>
+        ) : (
+          <ul className="cart-items">
+            {cart.map((item) => (
+              <li className="cart-item text-lg" key={item.id}>
+                {item.name} - ${item.price}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
